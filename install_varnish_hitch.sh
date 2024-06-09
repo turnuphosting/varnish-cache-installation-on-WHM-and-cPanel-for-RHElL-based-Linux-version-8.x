152350ec1f992a012be7ac98e9d8ff504c1ee15e
Ferguson230 <37697683+Ferguson230@users.noreply.github.com>
2024-06-09 00:43:19 -0400

    Update install_varnish_hitch.sh

--- install_varnish_hitch.sh
+++ install_varnish_hitch.sh
@@ -56,28 +56,23 @@ acl purge {\
 ' /etc/varnish/default.vcl
 
 # Include other configuration files after sub vcl_recv {
-sed -i '/sub vcl_recv {/a \\
+sed -i '/sub vcl_recv {/a \
     # Happens before we check if we have this in cache already.\
     #\
     # Typically you clean up the request here, removing cookies you don't need,\
     # rewriting the request, etc.\
-\
     # Remove empty query string parameters\
     # e.g.: www.example.com/index.html?\
     if (req.url ~ "\?$") {\
         set req.url = regsub(req.url, "\?$", "");\
     }\
-\
     # Remove port number from host header\
     set req.http.Host = regsub(req.http.Host, ":[0-9]+", "");\
-\
     # Sorts query string parameters alphabetically for cache normalization purposes\
    # set req.url = std.querysort(req.url);\
-\
     # Remove the proxy header to mitigate the httpoxy vulnerability\
     # See https://httpoxy.org/\
     unset req.http.proxy;\
-\
     # Add X-Forwarded-Proto header when using https\
     if(!req.http.X-Forwarded-Proto) {\
         if (proxy.is_ssl()) {\
@@ -86,7 +81,6 @@ sed -i '/sub vcl_recv {/a \\
             set req.http.X-Forwarded-Proto = "http";\
         }\
     }\
-\
     # Purge logic to remove objects from the cache.\
     # Tailored to the Proxy Cache Purge WordPress plugin\
     # See https://wordpress.org/plugins/varnish-http-purge/\
@@ -101,7 +95,6 @@ sed -i '/sub vcl_recv {/a \\
         ban("obj.http.x-url == " + req.url + " && obj.http.x-host == " + req.http.host);\
         return(synth(200, "Purged"));\
     }\
-\
     # Only handle relevant HTTP request methods\
     if (\
         req.method != "GET" &&\
@@ -115,7 +108,6 @@ sed -i '/sub vcl_recv {/a \\
     ) {\
         return (pipe);\
     }\
-\
     # Remove tracking query string parameters used by analytics tools\
     if (req.url ~ "(\?|&)(utm_source|utm_medium|utm_campaign|utm_content|gclid|cx|ie|cof|siteurl)=") {\
         set req.url = regsuball(req.url, "&(utm_source|utm_medium|utm_campaign|utm_content|gclid|cx|ie|cof|siteurl)=([A-z0-9_\-\.%25]+)", "");\
@@ -123,13 +115,11 @@ sed -i '/sub vcl_recv {/a \\
         set req.url = regsub(req.url, "\?&", "?");\
         set req.url = regsub(req.url, "\?$", "");\
     }\
-\
     # Only cache GET and HEAD requests\
     if (req.method != "GET" && req.method != "HEAD") {\
         set req.http.X-Cacheable = "NO:REQUEST-METHOD";\
         return(pass);\
     }\
-\
     # Mark static files with the X-Static-File header, and remove any cookies\
     # X-Static-File is also used in vcl_backend_response to identify static files\
     if (req.url ~ "^[^?]*\.(7z|avi|bmp|bz2|css|csv|doc|docx|eot|flac|flv|gif|gz|ico|jpeg|jpg|js|less|mka|mkv|mov|mp3|mp4|mpeg|mpg|odt|ogg|ogm|opus|otf|pdf|png|ppt|pptx|rar|rtf|svg|svgz|swf|tar|tbz|tgz|ttf|txt|txz|wav|webm|webp|woff|woff2|xls|xlsx|xml|xz|zip)(\?.*)?$") {\
@@ -137,7 +127,6 @@ sed -i '/sub vcl_recv {/a \\
         unset req.http.Cookie;\
         return(hash);\
     }\
-\
     # No caching of special URLs, logged in users and some plugins\
     if (\
         req.http.Cookie ~ "wordpress_(?!test_)[a-zA-Z0-9_]+|wp-postpass|comment_author_[a-zA-Z0-9_]+|woocommerce_cart_hash|woocommerce_items_in_cart|wp_woocommerce_session_[a-zA-Z0-9]+|wordpress_logged_in_|comment_author|PHPSESSID" ||\
@@ -182,7 +171,6 @@ sed -i '/sub vcl_recv {/a \\
              }\
         return(pass);\
     }\
-\
     # Remove any cookies left\
     unset req.http.Cookie;\
     return(hash);\
